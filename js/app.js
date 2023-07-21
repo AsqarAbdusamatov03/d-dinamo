b904e3e70fea8d549636c998ec33ef3e569d4fdb
AsqarAbdusamatov03 <asqarabdusamatov2@gmail.com>
2023-07-21 15:30:18 +0500

    page 1 polni tugatildi

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("nav-div").style.display = "block";
+    document.getElementById("close").style.display = "block";
+    document.getElementById("onclik").style.display= "none";
+}
+
+function closeNav() {
+    document.getElementById("nav-div").style.display = "none";
+    document.getElementById("close").style.display = "none";
+    document.getElementById("onclik").style.display= "block";
+}
